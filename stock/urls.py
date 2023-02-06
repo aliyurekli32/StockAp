30d2e2b5728614686158da32f77c8248cbe10551
aliyurekli32 <adamjesperblack@gmail.com>
2023-02-06 20:58:46 +0300

    add: all paths to urls

--- stock/urls.py
+++ stock/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from rest_framework import routers
-from .views import CategoryMVS,BrandView
+from .views import CategoryMVS,BrandView,FirmView,ProductView,PurchaseView,SalesView
 
 router = routers.DefaultRouter()
+
 router.register("categories", CategoryMVS)
 router.register("brands", BrandView)
+router.register("firms", FirmView)
+router.register("products", ProductView)
+router.register("purchases", PurchaseView)
+router.register("sales", SalesView)
 
 urlpatterns = [
     
