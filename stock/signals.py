aec97f3e024f239c37d3c99a42d4c35c8e475264
aliyurekli32 <adamjesperblack@gmail.com>
2023-02-06 10:58:28 +0300

    add: signals

---
+++ stock/signals.py
@@ -0,0 +1,7 @@
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from .models import Purchases, Sales
+
+@receiver(pre_save, sender=Purchases)
+def calculate_total_price(sender, instance, **kwargs):
+    instance.price_total = instance.quantity * instance.price
